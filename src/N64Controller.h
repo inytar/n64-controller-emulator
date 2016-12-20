bd546ded99bd6b03f7d726470d7338dfe718c821
inytar <pietpiet@fastmail.net>
2016-12-20 15:41:10 -0500

    Implementation with startTime
    Still not accurate enough.

--- src/N64Controller.h
+++ src/N64Controller.h
@@ -7,11 +7,16 @@ class N64Controller {
     public:
         N64Controller(int pin);
         void init(void);
-        void write(uint32_t);
+        /* void write(uint32_t); */
+        /* void write2(uint32_t); */
+        void writenoloop2(uint32_t);
+        /* void write3(bool[]); */
+        void writenoloop3(bool[]);
+        void testLoop(void);
     private:
         int _pin;
-        void write1(void);
-        void write0(void);
-        void writeStop(void);
+        void write1(unsigned);
+        void write0(unsigned);
+        void writeStop(unsigned);
 };
 #endif
