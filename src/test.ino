6fb5ee687250871a7f590480628d0c3722897a68
Hank <hankshorb@gmail.com>
2016-12-21 01:05:53 -0500

    Working cool fox animations!!!!

--- src/test.ino
+++ src/test.ino
@@ -1,83 +1,123 @@
 #include <Arduino.h>
 
 const int pin = 12;
-#define SEQLEN 32
+#define SEQLEN 132
 
-IntervalTimer timer;
-uint32_t seq;
+// Button refs
+#define A 0
+#define B 1
+#define Z 2
+#define S 3
+#define dU 4
+#define dD 5
+#define dL 6
+#define dR 7
+#define rst 8
+#define L 10
+#define R 11
+#define cU 12
+#define cD 13
+#define cL 14
+#define cR 15
+#define X 16
+#define Y 24
+
+int frame;
+volatile bool sentLast = true;
+
+bool seq[SEQLEN] = {false};
+bool emptyAction[SEQLEN] = {false};
+
 unsigned currBit = 0;
 bool flip = true;
 
 void setup() {
     init();
     Serial.begin(9600);
-    delay(100);
+    Serial.print("Starting Program");
+    Serial.println();
+    delay(500);
+    Serial.print("Ready");
+    Serial.println();
+    attachInterrupt(digitalPinToInterrupt(pin), writeSeq, FALLING);
 }
 
 void loop() {
-    seq = 5;
-    test2Loop();
-    delay(10);
+    if (sentLast) {
+        nextCommand();
+        noInterrupts();
+        sentLast = false;
+        interrupts();
+    }
 }
 
 void init() {
-    pinMode(pin, OUTPUT);
-    digitalWriteFast(pin, HIGH);
+    for (int i=3 ; i < SEQLEN - 4 ; i = i + 4) {
+        emptyAction[i] = true;
+    }
+    // Stop Bit
+    emptyAction[128] = false;
+    emptyAction[129] = false;
+    emptyAction[130] = true;
+    emptyAction[131] = true;
+    resetSeq();
+
+    frame = 0;
+
+    pinMode(pin, INPUT);
     // Set pin interupt
 }
 
-void writeNextHighLow() {
-    if ((1 << (SEQLEN - currBit - 1)) & seq) {
-        digitalWriteFast(pin, HIGH);
-    } else {
-        digitalWriteFast(pin, LOW);
+void writeSeq() {
+    noInterrupts();
+    delayMicroseconds(32);
+    pinMode(pin, OUTPUT);
+    for (int i=0 ; i<SEQLEN ; i++) {
+        if (seq[i]) {
+            digitalWrite(pin, HIGH);
+        } else {
+            digitalWrite(pin, LOW);
+        }
     }
-    currBit++;
-    if (currBit >= SEQLEN) {
-        timer.end();
-        currBit = 0;
+    pinMode(pin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(pin), writeSeq, FALLING);
+    sentLast = true;
+    interrupts();
+}
+
+void nextCommand() {
+    resetSeq();
+
+    if (frame == 0 || frame == 1){
+        pressButton(cU);
+    }
+    if (frame == 3 || frame == 4) {
+        pressButton(B);
+    }
+    setAxis(Y, -80);
+    setAxis(X, 40);
+    frame++;
+    if (frame > 13) {
+        frame = 0;
     }
 }
 
-void testLoop() {
-    timer.begin(writeNextHighLow, 2);
-    timer.priority(0);
+void pressButton(int button) {
+    seq[button * 4 + 1] = true;
+    seq[button * 4 + 2] = true;
 }
 
-void writeHigh() {
-    digitalWrite(pin, HIGH);
-    /*digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);
-    digitalWriteFast(pin, HIGH);*/
+void setAxis(int axis, int8_t val) {
+    for (int i=0 ; i<8 ; i++) {
+        bool bit = (1<<(7-i)) & val;
+        if (bit) {
+            pressButton(axis + i);
+        }
+    }
 }
 
-void writeLow() {
-    digitalWrite(pin, LOW);
-    /*digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);
-    digitalWriteFast(pin, LOW);*/
-}
-
-void test2Loop() {
-    writeLow();
-    writeHigh();
-    writeLow();
-    writeHigh();
-    writeLow();
-    writeHigh();
+void resetSeq() {
+    for (int i=0 ; i<SEQLEN ; i++) {
+        seq[i] = emptyAction[i];
+    }
 }
